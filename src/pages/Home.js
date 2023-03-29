1bf82dace6ae35da5af6b5c4b18cff3be6fefa37
KimNamGyu <103492768+KingNamGyu@users.noreply.github.com>
2023-03-29 20:23:46 +0900

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,7 +20,6 @@ const Home = () => {
           <div className="name">-김남규-</div>
           <h1>웹 포트폴리오</h1>
           <h2>나침반은 사람들을 올바른 길로 안내합니다.</h2>
-          <h2>저는 그런 웹 개발자가 되고 싶습니다.</h2>
         </div>
       </div>
       <Footer />
